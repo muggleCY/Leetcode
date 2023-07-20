333b052ebbbcd0b4f02455da762cec36e2df4794
muggleCY <695632930@qq.com>
2023-07-20 15:50:38 +0800

    解题修改

--- 剑指 Offer 57. 和为s的两个数字.js
+++ 剑指 Offer 57. 和为s的两个数字.js
@@ -4,16 +4,16 @@
  * @return {number[]}
  * 双指针 左右指针相加 如果大于target 右指针向左移 小于左指针向右移 相等则返回
  */
-var twoSum = function (nums, target) {
+var twoSum = function(nums, target) {
     let left = 0
-    let right = nums.length - 1
-    while (left <= right) {
-        if (nums[left] + nums[right] > target) {
-            right--
-        } else if (nums[left] + nums[right] < target) {
-            left--
-        } else {
-            return [nums[left], nums[right]]
+    let right = nums.length -1
+    while(left < right){
+        if(nums[left]+ nums[right] > target){
+            right --
+        }else if(nums[left]+ nums[right] < target){
+            left ++
+        }else{
+            return [nums[left],nums[right]]
         }
     }
     return null
