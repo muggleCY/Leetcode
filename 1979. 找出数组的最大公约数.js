7cd6a5fa340e8182da0524bdd39d2bec43250de2
muggleCY <695632930@qq.com>
2024-03-29 10:09:38 +0800

    注释

--- 1979. 找出数组的最大公约数.js
+++ 1979. 找出数组的最大公约数.js
@@ -5,7 +5,7 @@
 const findGCD = function (nums) {
     let max = Math.max(...nums)
     let min = Math.min(...nums)
-    // 辗转相除法 求 最大公约数
+    // 辗转相除法(图解 https://blog.csdn.net/2201_75314884/article/details/131206814) 求 最大公约数
     function gcd(a, b) {
         let r = a % b // 余数
         return b === 0 ? a : gcd(b, r)
